e2b260b25950fb9c4af757957c12b8787bd04d62
Theodore Ehrenborg <theodore.ehrenborg@gmail.com>
2025-01-18 15:07:28 +0000

    Remove python files

--- tests/test_imports.py
+++ tests/test_imports.py
@@ -1,6 +1,2 @@
 def test_imports():
-    import sae_hacking.call_openai
-    import sae_hacking.gather_high_activations
-    import sae_hacking.gather_high_activations_llm
-    import sae_hacking.steer
     import sae_hacking.train_sae  # noqa: F401
