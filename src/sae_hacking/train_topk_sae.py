44f96e3714e1a27a1cda5847c3d9731c404c17de
Theodore Ehrenborg <theodore.ehrenborg@gmail.com>
2025-01-19 17:17:33 +0000

    Remove L1 penalty from topk SAE code

--- src/sae_hacking/train_topk_sae.py
+++ src/sae_hacking/train_topk_sae.py
@@ -47,7 +47,6 @@ def setup(
 @beartype
 def make_parser() -> ArgumentParser:
     parser = make_base_parser()
-    parser.add_argument("--l1_coefficient", type=float, default=0.0)
     parser.add_argument(
         "--only_count_tokens",
         action="store_true",
@@ -80,11 +79,9 @@ def main(user_args: Namespace):
         optimizer.zero_grad()
         activation = get_llm_activation(llm, example, user_args)
         norm_act = normalize_activations(activation)
-        sae_act, feat_magnitudes = sae(norm_act)
+        sae_act = sae(norm_act)
         rec_loss = get_reconstruction_loss(norm_act, sae_act)
-        l1_penalty, nonzero_proportion = get_l1_penalty_nonzero(feat_magnitudes)
-        loss = rec_loss + user_args.l1_coefficient * l1_penalty
-        loss.backward()
+        rec_loss.backward()
         optimizer.step()
         if step % 5000 == 0:
             torch.save(sae.state_dict(), f"{output_dir}/{step}.pt")
@@ -97,9 +94,9 @@ def main(user_args: Namespace):
         writer.add_scalar("norm act std/train", norm_act.std(), step)
         writer.add_scalar("sae act mean/train", sae_act.mean(), step)
         writer.add_scalar("sae act std/train", sae_act.std(), step)
-        writer.add_scalar("Total loss/train", loss, step)
+        writer.add_scalar("Total loss/train", rec_loss, step)
         writer.add_scalar(
-            "Total loss per element/train", loss / torch.numel(norm_act), step
+            "Total loss per element/train", rec_loss / torch.numel(norm_act), step
         )
         writer.add_scalar("Reconstruction loss/train", rec_loss, step)
         writer.add_scalar(
@@ -107,12 +104,6 @@ def main(user_args: Namespace):
             rec_loss / torch.numel(norm_act),
             step,
         )
-        writer.add_scalar("L1 penalty/train", l1_penalty, step)
-        writer.add_scalar(
-            "L1 penalty per element/train", l1_penalty / torch.numel(norm_act), step
-        )
-        writer.add_scalar("L1 penalty strength", user_args.l1_coefficient, step)
-        writer.add_scalar("Proportion of nonzero features", nonzero_proportion, step)
 
     writer.close()
 
@@ -122,18 +113,8 @@ def get_reconstruction_loss(
     act: Float[torch.Tensor, "1 seq_len 768"],
     sae_act: Float[torch.Tensor, "1 seq_len 768"],
 ) -> Float[torch.Tensor, ""]:
+    # TODO DRY this
     return ((act - sae_act) ** 2).sum()
-
-
-@jaxtyped(typechecker=beartype)
-def get_l1_penalty_nonzero(
-    feat_magnitudes: Float[torch.Tensor, "1 seq_len sae_hidden_dim"],
-) -> tuple[Float[torch.Tensor, ""], Float[torch.Tensor, ""]]:
-    # Sum over the SAE features (i.e. a 1-norm)
-    # And then sum over seq_len and batch
-    l1 = torch.linalg.vector_norm(feat_magnitudes, ord=1)
-    l0 = torch.linalg.vector_norm(feat_magnitudes, ord=0)
-    return l1, l0 / torch.numel(feat_magnitudes)
 
 
 if __name__ == "__main__":
