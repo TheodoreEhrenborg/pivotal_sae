cf46ba7b1a0bd0f5f5dcebb22a66cd452214c0fe
Theodore Ehrenborg <theodore.ehrenborg@gmail.com>
2025-01-19 21:31:02 +0000

    Fix up tensorboard script

--- run.sh
+++ run.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env sh
+if [ -z "$command" ]; then
+	command=/bin/bash
+fi
 docker run -it --rm \
     $@ \
     -v $HOME/.cache/huggingface:/root/.cache/huggingface \
@@ -6,4 +9,4 @@ docker run -it --rm \
     -v $HOME/.cache/uv:/root/.cache/uv \
     -v $HOME/.local/share/uv:/root/.local/share/uv \
     $(cat docker_name) \
-    sh -c "/bin/bash"
+    sh -c "$command"
