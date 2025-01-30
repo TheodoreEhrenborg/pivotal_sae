1dc990a65ae24c1f9af8f4245a6cefbd031437ea
Theodore Ehrenborg <theodore.ehrenborg@gmail.com>
2025-01-30 21:17:13 +0000

    Make tensorboard port configurable

--- run_tb.sh
+++ run_tb.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 set -ue
-command="/root/.cargo/bin/uv run tensorboard --logdir=/results/ --port 6010 --host 0.0.0.0"
-command=$command ./run.sh -v "$(realpath $1)":/results -p 6010:6010
+PORT=${PORT:-6010}
+command="/root/.cargo/bin/uv run tensorboard --logdir=/results/ --port $PORT --host 0.0.0.0"
+command=$command ./run.sh -v "$(realpath $1)":/results -p $PORT:$PORT
