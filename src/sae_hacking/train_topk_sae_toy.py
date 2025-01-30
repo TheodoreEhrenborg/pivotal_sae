7b982d21e05e16865cafc042e2365e75d3986347
Theodore Ehrenborg <theodore.ehrenborg@gmail.com>
2025-01-30 21:05:48 +0000

    Copy train_topk_sae.py, modify for toy dataset

---
+++ src/sae_hacking/train_topk_sae_toy.py
@@ -0,0 +1,89 @@
+#!/usr/bin/env python3
+import time
+from argparse import ArgumentParser, Namespace
+
+import torch
+from beartype import beartype
+from coolname import generate_slug
+from jaxtyping import Float, jaxtyped
+from torch.utils.tensorboard import SummaryWriter
+from tqdm import trange
+
+from sae_hacking.common.sae import TopkSparseAutoEncoder, TopkSparseAutoEncoder2Child
+from sae_hacking.common.setting_up import make_base_parser
+from sae_hacking.common.toy_dataset import ToyDataset
+
+
+@beartype
+def setup(
+    sae_hidden_dim: int,
+    cuda: bool,
+    no_internet: bool,
+    hierarchical: bool,
+) -> TopkSparseAutoEncoder | TopkSparseAutoEncoder2Child:
+    SAEClass = TopkSparseAutoEncoder2Child if hierarchical else TopkSparseAutoEncoder
+    print(SAEClass)
+    sae = SAEClass(sae_hidden_dim)
+    if cuda:
+        sae.cuda()
+    return sae
+
+
+@beartype
+def make_parser() -> ArgumentParser:
+    parser = make_base_parser()
+    parser.add_argument("--hierarchical", action="store_true")
+    return parser
+
+
+@beartype
+def main(user_args: Namespace):
+    output_dir = f"/results/{time.strftime('%Y%m%d-%H%M%S')}{generate_slug()}"
+    print(f"Writing to {output_dir}")
+    writer = SummaryWriter(output_dir)
+    dataset = ToyDataset(seed=1)
+
+    sae = setup(
+        user_args.sae_hidden_dim,
+        user_args.cuda,
+        False,
+        user_args.hierarchical,
+    )
+
+    lr = 1e-5
+    optimizer = torch.optim.Adam(sae.parameters(), lr=lr)
+
+    for step in trange(user_args.max_step):
+        example = dataset.generate()
+        optimizer.zero_grad()
+        reconstructed = sae(example)
+        rec_loss = get_reconstruction_loss(reconstructed, example)
+        rec_loss.backward()
+        optimizer.step()
+        if step % 5000 == 0:
+            torch.save(sae.state_dict(), f"{output_dir}/{step}.pt")
+
+        writer.add_scalar("lr", lr, step)
+        writer.add_scalar("sae_hidden_dim", user_args.sae_hidden_dim, step)
+        writer.add_scalar("Total loss/train", rec_loss, step)
+        writer.add_scalar("Reconstruction loss/train", rec_loss, step)
+        writer.add_scalar(
+            "Reconstruction loss per element/train",
+            rec_loss / torch.numel(example),
+            step,
+        )
+
+    writer.close()
+
+
+@jaxtyped(typechecker=beartype)
+def get_reconstruction_loss(
+    act: Float[torch.Tensor, "1 seq_len 768"],
+    sae_act: Float[torch.Tensor, "1 seq_len 768"],
+) -> Float[torch.Tensor, ""]:
+    # TODO DRY this
+    return ((act - sae_act) ** 2).sum()
+
+
+if __name__ == "__main__":
+    main(make_parser().parse_args())
