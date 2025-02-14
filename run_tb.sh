00d447d5a1cf93846b974932acf8aeebc6e1614c
Theodore Ehrenborg <theodore.ehrenborg@gmail.com>
2025-02-14 14:14:23 +0000

    Fix tb script, add docstring, add quotes

--- run_tb.sh
+++ run_tb.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
-set -ue
+set -e
 PORT=${PORT:-6010}
-command="/root/.cargo/bin/uv run tensorboard --logdir=/results/ --port $PORT --host 0.0.0.0"
-command=$command ./run.sh -v "$(realpath $1)":/results -p $PORT:$PORT
+if [ -z "$1" ]; then
+    echo "Usage: ./run_tb.sh /path/to/results"
+    exit 1
+fi
+command="/root/.local/bin/uv run tensorboard --logdir=/results/ --port $PORT --host 0.0.0.0"
+command=$command ./run.sh -v "$(realpath "$1")":/results -p "$PORT":"$PORT"
