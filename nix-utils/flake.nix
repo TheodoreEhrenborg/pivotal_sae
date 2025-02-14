f260b7f87137e1bbe9275f4466cb3a330a82b1b1
Theodore Ehrenborg <theodore.ehrenborg@gmail.com>
2025-02-14 11:21:08 +0000

    Remove unnecessary direnv deps

--- nix-utils/flake.nix
+++ nix-utils/flake.nix
@@ -18,14 +18,8 @@
         with pkgs; {
           devShells.default = mkShell {
             buildInputs = [
-              stdenv.cc.cc.lib
-              cudatoolkit
-              julia
-              cudaPackages.cuda_sanitizer_api
-              cudaPackages.cuda_cudart
               nvtop
             ];
-            LD_LIBRARY_PATH = "${stdenv.cc.cc.lib}/lib:/run/opengl-driver/lib:${cudaPackages.cuda_cudart}/lib";
           };
         }
     );
