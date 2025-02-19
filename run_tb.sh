2a8194e9ac0fb7fb599cb965e6192368beeb18ed
Theodore Ehrenborg <theodore.ehrenborg@gmail.com>
2025-02-19 17:14:03 +0000

    Expand run_tb to handle symlinks

--- run_tb.sh
+++ run_tb.sh
@@ -2,8 +2,10 @@
 set -e
 PORT=${PORT:-6010}
 if [ -z "$1" ]; then
-    echo "Usage: ./run_tb.sh /path/to/results"
-    exit 1
+	echo "Usage: ./run_tb.sh /path/to/results <other docker args>"
+	exit 1
 fi
-command="/root/.local/bin/uv run tensorboard --logdir=/results/ --port $PORT --host 0.0.0.0"
-command=$command ./run.sh -v "$(realpath "$1")":/results -p "$PORT":"$PORT"
+full_path=$(realpath "$1")
+shift 1
+command="/root/.local/bin/uv run tensorboard --logdir=$full_path --port $PORT --host 0.0.0.0"
+command=$command ./run.sh -v "$full_path":"$full_path" -p "$PORT":"$PORT" "$@"
