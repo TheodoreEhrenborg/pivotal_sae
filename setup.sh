4de597f520713f2dc6c2bbccaa2d88c66a8ad11f
Theodore Ehrenborg <theodore.ehrenborg@gmail.com>
2025-02-20 16:00:28 +0000

    Changes to support saving hash

--- setup.sh
+++ setup.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 set -e
 apt-get update
-apt-get install -y fish curl nvtop htop
+apt-get install -y fish curl nvtop htop git
+git config --global --add safe.directory /code
 curl -LsSf https://astral.sh/uv/install.sh | sh
