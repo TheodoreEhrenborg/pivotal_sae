7d360fad85c567425486018d3215cb7d31e74089
Theodore Ehrenborg <theodore.ehrenborg@gmail.com>
2025-02-21 16:39:16 +0000

    Try plotting a dendrogram

--- src/sae_hacking/gemma_cluster.py
+++ src/sae_hacking/gemma_cluster.py
@@ -3,9 +3,11 @@ import asyncio
 from argparse import ArgumentParser, Namespace
 
 import aiohttp
+import matplotlib.pyplot as plt
 import numpy as np
 from beartype import beartype
 from huggingface_hub import hf_hub_download
+from scipy.cluster import hierarchy
 from sklearn.cluster import AgglomerativeClustering
 
 # Gemma-scope based on https://colab.research.google.com/drive/17dQFYUYnuKnP6OwQPH9v_GSYUW5aj-Rp
@@ -24,7 +26,11 @@ async def get_description_async(idx: int, session: aiohttp.ClientSession) -> str
     url = f"https://www.neuronpedia.org/api/feature/gemma-2-2b/20-gemmascope-res-16k/{idx}"
     async with session.get(url) as response:
         data = await response.json()
-        return data["explanations"][0]["description"]
+        try:
+            return data["explanations"][0]["description"]
+        except:
+            print(data)
+            raise
 
 
 @beartype
@@ -48,6 +54,32 @@ def main(args: Namespace) -> None:
         decoder_vectors_EM = decoder_vectors_EM[0 : args.abridge]
     E = decoder_vectors_EM.shape[0]
     print(f"{decoder_vectors_EM.shape=}")
+    Z = hierarchy.linkage(decoder_vectors_EM, "complete")
+    plt.figure()
+    dn = hierarchy.dendrogram(
+        Z,
+        p=4,
+        truncate_mode="level",
+        labels=[f"label {i}" for i in range(E)],
+        no_plot=True
+    )
+    if True:
+        for i in dn['icoord']:
+            for j in range(len(i)-1):
+                plt.plot([i[j], i[j+1]], [dn['dcoord'][dn['icoord'].index(i)][j],
+                        dn['dcoord'][dn['icoord'].index(i)][j+1]], 'k-', linewidth=0.5)
+        plt.xticks(dn['leaves'],
+          [f"label {i}" for i in dn['leaves']],
+          fontsize=6)
+
+    # plt.tick_params(axis='x', labelsize=1)
+    plt.savefig(
+        "/results/tmp.png",
+        dpi=300,
+        bbox_inches="tight",
+    )
+    plt.close()
+    exit()
 
     cluster_model = AgglomerativeClustering(
         distance_threshold=None,
@@ -60,6 +92,7 @@ def main(args: Namespace) -> None:
     n_clusters = len(np.unique(cluster_labels))
     print(f"\nNumber of clusters: {n_clusters}")
     print("Getting descriptions from Neuronpedia...")
+    exit()
     descriptions = asyncio.run(get_all_descriptions(list(range(E))))
     for i in range(n_clusters):
         cluster_size = np.sum(cluster_labels == i)
