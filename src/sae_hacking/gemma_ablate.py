1c70c3d9e5e640bce03069082fa279b2febbb080
Theodore Ehrenborg <theodore.ehrenborg@gmail.com>
2025-02-25 13:56:11 +0000

    Copy in code from notebook
    https://colab.research.google.com/github/jbloomAus/SAELens/blob/main/tutorials/tutorial_2_0.ipynb

--- src/sae_hacking/gemma_ablate.py
+++ src/sae_hacking/gemma_ablate.py
@@ -14,6 +14,53 @@ from scipy.cluster import hierarchy
 def make_parser() -> ArgumentParser:
     parser = ArgumentParser()
     return parser
+from transformer_lens.utils import test_prompt
+from functools import partial
+
+
+def test_prompt_with_ablation(model, sae, prompt, answer, ablation_features):
+    def ablate_feature_hook(feature_activations, hook, feature_ids, position=None):
+        if position is None:
+            feature_activations[:, :, feature_ids] = 0
+        else:
+            feature_activations[:, position, feature_ids] = 0
+
+        return feature_activations
+
+    ablation_hook = partial(ablate_feature_hook, feature_ids=ablation_features)
+
+    model.add_sae(sae)
+    hook_point = sae.cfg.hook_name + ".hook_sae_acts_post"
+    model.add_hook(hook_point, ablation_hook, "fwd")
+
+    test_prompt(prompt, answer, model)
+
+    model.reset_hooks()
+    model.reset_saes()
+
+
+# Example usage in a notebook:
+
+# Assume model and sae are already defined
+
+# Choose a feature to ablate
+
+model.reset_hooks(including_permanent=True)
+prompt = "In the beginning, God created the heavens and the"
+answer = "earth"
+test_prompt(prompt, answer, model)
+
+
+# Generate text with feature ablation
+print("Test Prompt with feature ablation and no error term")
+ablation_feature = 16873  # Replace with any feature index you're interested in. We use the religion feature
+sae.use_error_term = False
+test_prompt_with_ablation(model, sae, prompt, answer, ablation_feature)
+
+print("Test Prompt with feature ablation and error term")
+ablation_feature = 16873  # Replace with any feature index you're interested in. We use the religion feature
+sae.use_error_term = True
+test_prompt_with_ablation(model, sae, prompt, answer, ablation_feature)
 
 
 @beartype
