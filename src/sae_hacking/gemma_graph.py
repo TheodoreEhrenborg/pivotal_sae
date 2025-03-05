d92831b9280f2caa6591667bf84ad1fd03a872cc
Theodore Ehrenborg <theodore.ehrenborg@gmail.com>
2025-03-05 11:31:23 +0000

    Add script that just graphs

---
+++ src/sae_hacking/gemma_graph.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+from argparse import ArgumentParser, Namespace
+from pathlib import Path
+
+from beartype import beartype
+
+from sae_hacking.graph_network import graph_ablation_matrix
+from sae_hacking.json_utils import load_dict_with_tensors_from_json
+
+
+@beartype
+def make_parser() -> ArgumentParser:
+    parser = ArgumentParser()
+    parser.add_argument("--ablater-sae-neuronpedia-id", required=True)
+    parser.add_argument("--reader-sae-neuronpedia-id", required=True)
+    parser.add_argument("--n-edges", type=int, default=1000)
+    parser.add_argument("--output-dir", type=Path, required=True)
+    parser.add_argument("--json-path", type=Path, required=True)
+    return parser
+
+
+@beartype
+def main(args: Namespace) -> None:
+    ablation_results = load_dict_with_tensors_from_json(args.json_path)
+
+    graph_ablation_matrix(
+        ablation_results,
+        args.ablater_sae_neuronpedia_id,
+        args.reader_sae_neuronpedia_id,
+        args.output_dir,
+        args.n_edges,
+    )
+
+
+if __name__ == "__main__":
+    main(make_parser().parse_args())
