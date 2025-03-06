f94e2bd4bb8610724b3588daede2402868209325
Theodore Ehrenborg <theodore.ehrenborg@gmail.com>
2025-03-06 10:33:02 +0000

    Skip the temporary file

--- src/sae_hacking/safetensor_utils.py
+++ src/sae_hacking/safetensor_utils.py
@@ -1,10 +1,9 @@
 #!/usr/bin/env python3
-import os
 
 import torch
 import zstandard
 from beartype import beartype
-from safetensors.torch import save_file
+from safetensors.torch import save
 
 
 @beartype
@@ -17,26 +16,17 @@ def save_dict_with_tensors(tensor_dict: dict, save_path: str) -> None:
         tensor_dict: Dictionary containing tensors
         save_path: Path to save the safetensors file
     """
+    # Make sure the save_path has .zst extension
+    assert save_path.endswith(".zst")
 
     for _, value in tensor_dict.items():
         assert isinstance(value, torch.Tensor)
 
-    # Make sure the save_path has .zst extension
-    assert save_path.endswith(".zst")
-    # Use a temporary file for the uncompressed version
-    safetensors_path = save_path[:-4]  # Remove .zst
-
     # Save tensors with metadata
-    save_file(tensor_dict, safetensors_path)
-
-    with open(safetensors_path, "rb") as f_in:
-        uncompressed_data = f_in.read()
+    uncompressed_data = save(tensor_dict)
 
     compressor = zstandard.ZstdCompressor()
     compressed_data = compressor.compress(uncompressed_data)
 
     with open(save_path, "wb") as f_out:
         f_out.write(compressed_data)
-
-    # Remove the temporary uncompressed file
-    os.remove(safetensors_path)
