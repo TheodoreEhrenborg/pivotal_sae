351c67c11573c040fd91bc8bde2322709d195530
Theodore Ehrenborg <theodore.ehrenborg@gmail.com>
2025-03-06 10:48:10 +0000

    Convert json format to safetensors format

---
+++ src/sae_hacking/convert_json_to_safetensors.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+from argparse import ArgumentParser, Namespace
+
+from beartype import beartype
+
+from sae_hacking.json_utils import load_dict_with_tensors_from_json
+from sae_hacking.safetensor_utils import save_dict_with_tensors
+
+
+@beartype
+def make_parser() -> ArgumentParser:
+    parser = ArgumentParser()
+    parser.add_argument("input-path")
+    parser.add_argument("output-path")
+    return parser
+
+
+@beartype
+def main(args: Namespace) -> None:
+    tensors = load_dict_with_tensors_from_json(args.input_path)
+    save_dict_with_tensors(tensors, args.output_path)
+
+
+if __name__ == "__main__":
+    main(make_parser().parse_args())
