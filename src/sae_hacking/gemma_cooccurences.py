3afe35f6d83b47faffd1d6fdb58654b81e5763c7
Theodore Ehrenborg <theodore.ehrenborg@gmail.com>
2025-03-19 17:35:07 +0000

    Remove dead code

--- src/sae_hacking/gemma_cooccurences.py
+++ src/sae_hacking/gemma_cooccurences.py
@@ -114,25 +114,5 @@ def gather_co_occurrences2(ablator_acts_1Se) -> torch.Tensor:
     return these_cooccurrences_ee.cpu()
 
 
-@beartype
-def gather_co_occurrences(ablator_acts_1Se) -> torch.Tensor:
-    e = ablator_acts_1Se.shape[2]
-    these_cooccurrences_ee = torch.zeros(e, e)
-    # Update co-occurrence matrix for ablator features
-    # For each position in the sequence
-    for pos in range(ablator_acts_1Se.shape[1]):
-        # Get feature activations at this position
-        pos_acts_e = ablator_acts_1Se[0, pos]
-        # Find which features are active (> 0)
-        active_features = torch.where(pos_acts_e > 0)[0]
-
-        # Update co-occurrence counts for each pair of active features
-        for i in range(len(active_features)):
-            for j in range(len(active_features)):
-                feat1, feat2 = active_features[i].item(), active_features[j].item()
-                these_cooccurrences_ee[feat1, feat2] += 1
-    return these_cooccurrences_ee
-
-
 if __name__ == "__main__":
     main(make_parser().parse_args())
