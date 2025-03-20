604c1bef1e8f3b3397faacea5cb1519586fdb086
Theodore Ehrenborg <theodore.ehrenborg@gmail.com>
2025-03-20 12:42:39 +0000

    Finish test

--- tests/test_gemma_cooccurrences.py
+++ tests/test_gemma_cooccurrences.py
@@ -1,7 +1,9 @@
+import torch
+
 from sae_hacking.gemma_cooccurrences import generate_prompts
 
 
 def test_generate_prompts():
     d = generate_prompts("google/gemma-2-2b", 10, 100, "monology/pile-uncopyrighted", 2)
     for batch in d:
-        print(batch["abridged_tensor"].shape)
+        assert batch["abridged_tensor"].shape == torch.Size([2, 100])
