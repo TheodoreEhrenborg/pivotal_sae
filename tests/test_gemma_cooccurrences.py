984e2eb6e97e2b2cb9bb5e93d292f9121f57d508
Theodore Ehrenborg <theodore.ehrenborg@gmail.com>
2025-03-24 16:41:20 +0000

    Fix a test's import

--- tests/test_gemma_cooccurrences.py
+++ tests/test_gemma_cooccurrences.py
@@ -1,9 +1,11 @@
 import torch
 
-from sae_hacking.gemma_cooccurrences import generate_prompts
+from sae_hacking.gemma_utils import generate_prompts2
 
 
 def test_generate_prompts():
-    d = generate_prompts("google/gemma-2-2b", 10, 100, "monology/pile-uncopyrighted", 2)
+    d = generate_prompts2(
+        "google/gemma-2-2b", 10, 100, "monology/pile-uncopyrighted", 2
+    )
     for batch in d:
         assert batch["abridged_tensor"].shape == torch.Size([2, 100])
